51a3fb0c6eadf0a541f0c294578b3d9b5fb9f014
Lucas Muniz <lucaslima1048@gmail.com>
2023-10-26 15:35:15 -0300

    add some tests for create board and implements

--- spec/requests/boards_spec.rb
+++ spec/requests/boards_spec.rb
@@ -14,4 +14,32 @@ RSpec.describe "Boards", type: :request do
       expect(response).to have_http_status(200)
     end
   end
+
+  describe "POST create" do
+    context "with valid params" do
+      it "creates a new board and redirect" do
+        expect do
+          post boards_path, params: {
+            board: {
+              name: "New board"
+            }
+          }
+        end.to change { Board.count }.by(1)
+        expect(response).to have_http_status(:redirect)
+      end
+    end
+
+    context "with invalid params" do
+      it "not creates a new board and not redirect" do
+        expect do
+          post boards_path, params: {
+            board: {
+              name: ""
+            }
+          }
+        end.not_to change { Board.count }
+        expect(response).to have_http_status(:success)
+      end
+    end
+  end
 end
