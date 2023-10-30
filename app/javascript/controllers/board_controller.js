7bb7c85077440ddf79d8f576a979fecb791e8d14
Lucas Muniz <lucaslima1048@gmail.com>
2023-10-30 13:38:41 -0300

    clean the code and add dynamic url

--- app/javascript/controllers/board_controller.js
+++ app/javascript/controllers/board_controller.js
@@ -1,9 +1,16 @@
 import { Controller } from "@hotwired/stimulus"
 import axios from 'axios'
-import { map, get, isNull } from 'lodash-es'
+import { map, get, isNull, sample } from 'lodash-es'
 
 export default class extends Controller {
   HEADERS = { 'ACCEPT': 'application/json' }
+  BACKGROUND_COLORS = ['bg-green-700', 'bg-red-700', 'bg-blue-700']
+
+  connect() {
+    axios.get(this.element.dataset.apiUrl, { headers: this.HEADERS }).then((response) => {
+        this.buildKanban(this.buildBoards(response['data']))
+    })
+  }
 
   buildBoards(boardsData){
     return map(boardsData['data'], (board) => {
@@ -21,67 +28,22 @@ export default class extends Controller {
         return {
             "id": get(item, 'id'),
             "title": get(item, 'attributes.title'),
-            "class": this.buildClassList(get(item, 'attributes.class_list'))
+            "class": this.buildClassList()
         }
     })
   }
 
-  buildClassList(classList) {
-
-    if(isNull(classList)){
-        return ""
-    }
-
-    return classList.split(' ').join(', ')
+  buildClassList() {
+    return `text-white, ${sample(this.BACKGROUND_COLORS)}`
   }
 
-  buildKanban() {
-
-  }
-
-  connect() {
-
-
-    axios.get('/api/boards/1/lists', { headers: this.HEADERS }).then((response) => {
-        console.log('list response', response)
-
-        const boards = this.buildBoards(response['data']);
-
-        console.log('boards', boards)
-
-        var kanban = new jKanban({
-            element          : '.board',                                           // selector of the kanban container
-            gutter           : '15px',                                       // gutter of the board
-            widthBoard       : '250px',                                      // width of the board
-            responsivePercentage: false,                                    // if it is true I use percentage in the width of the boards and it is not necessary gutter and widthBoard
-            dragItems        : true,                                         // if false, all items are not draggable
-            boards           : boards,                                           // json of boards
-            dragBoards       : true,                                         // the boards are draggable, if false only item can be dragged
-            itemAddOptions: {
-                enabled: false,                                              // add a button to board for easy item creation
-                content: '+',                                                // text or html content of the board button   
-                class: 'kanban-title-button btn btn-default btn-xs',         // default class of the button
-                footer: false                                                // position the button on footer
-            },
-            itemHandleOptions: {
-                enabled             : false,                                 // if board item handle is enabled or not
-                handleClass         : "item_handle",                         // css class for your custom item handle
-                customCssHandler    : "drag_handler",                        // when customHandler is undefined, jKanban will use this property to set main handler class
-                customCssIconHandler: "drag_handler_icon",                   // when customHandler is undefined, jKanban will use this property to set main icon handler class. If you want, you can use font icon libraries here
-                customHandler       : "<span class='item_handle'>+</span> %title% "  // your entirely customized handler. Use %title% to position item title 
-                                                                                    // any key's value included in item collection can be replaced with %key%
-            },
-            click            : function (el) {},                             // callback when any board's item are clicked
-            context          : function (el, event) {},                      // callback when any board's item are right clicked
-            dragEl           : function (el, source) {},                     // callback when any board's item are dragged
-            dragendEl        : function (el) {},                             // callback when any board's item stop drag
-            dropEl           : function (el, target, source, sibling) {},    // callback when any board's item drop in a board
-            dragBoard        : function (el, source) {},                     // callback when any board stop drag
-            dragendBoard     : function (el) {},                             // callback when any board stop drag
-            buttonClick      : function(el, boardId) {},                     // callback when the board's button is clicked
-            propagationHandlers: [],                                         // the specified callback does not cancel the browser event. possible values: "click", "context"
-        })
+  buildKanban(boards) {
+    new jKanban({
+        element: '#board',
+        boards: boards,
+        itemAddOptions:{
+            enabled: true,
+        }
     })
-
   }
 }
